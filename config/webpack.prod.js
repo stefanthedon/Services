59d33c4bdc504def482a224757ce35d11ff78df2
Donald Parminter Lokong <don.parminter@gmail.com>
2018-07-03 13:52:33 +0300

    #XD

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -3,6 +3,7 @@ const merge = require('webpack-merge');
 
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(commonConfig, {
   mode: 'production',
@@ -12,15 +13,51 @@ module.exports = merge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
-    })
-  ]
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          output: {
+            comments: false
+          },
+          compress: {
+            unsafe_comps: true,
+            properties: true,
+            keep_fargs: false,
+            pure_getters: true,
+            collapse_vars: true,
+            unsafe: true,
+            warnings: false,
+            screw_ie8: true,
+            sequences: true,
+            dead_code: true,
+            drop_debugger: true,
+            comparisons: true,
+            conditionals: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            hoist_funs: true,
+            if_return: true,
+            join_vars: true,
+            cascade: true,
+            drop_console: true
+          }
+        }
+      }),
+    ]
+  }
+
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     compressor: {
+  //       warnings: false,
+  //       screw_ie8: true
+  //     },
+  //     output: {
+  //       comments: false
+  //     }
+  //   })
+  // ]
 });
