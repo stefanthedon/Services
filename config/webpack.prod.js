4a195e4e6de6ccb9ea425cf72466b9e61b5ca6f1
Donald Parminter Lokong <don.parminter@gmail.com>
2018-07-03 13:37:13 +0300

    #XD

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,34 +12,15 @@ module.exports = merge(commonConfig, {
     chunkFilename: '[id].[hash].chunk.js'
   },
 
-  optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendor',
-                    chunks: 'all'
-                }
-            }
-        }
-    },
-
-  // plugins: [
-  //   new webpack.optimize.UglifyJsPlugin({
-  //     compressor: {
-  //       warnings: false,
-  //       screw_ie8: true
-  //     },
-  //     output: {
-  //       comments: false
-  //     }
-  //   })
-  // ]
   plugins: [
-        // ensure that we get a production build of any dependencies
-        // this is primarily for React, where this removes 179KB from the bundle
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': '"production"'
-        })
-    ]
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false,
+        screw_ie8: true
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
 });
